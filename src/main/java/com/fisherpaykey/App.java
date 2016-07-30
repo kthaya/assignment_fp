3926f2dd3b2749ad2ab812f5e8463514ced1d586
kthaya <kthaya24@gmail.com>
2016-07-30 19:04:08 +1200

    Bootstrap project

---
+++ src/main/java/com/fisherpaykey/App.java
@@ -0,0 +1,15 @@
+package com.fisherpaykey;
+
+/**
+ * Client App
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+            	
+    	
+    }    
+    
+}
---
+++ src/test/java/com/fisherpaykel/AppTest.java
@@ -0,0 +1,15 @@
+package com.fisherpaykel;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    public void test()
+    {
+
+    }
+    
+}
